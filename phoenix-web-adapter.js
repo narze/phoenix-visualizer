14f224027290b7f5dd1fdf112f3b2682cdf4e21f
Manassarn Manoonchai <manassarn@gmail.com>
2020-06-21 20:36:48 +0700

    :sparkles: Add PhoenixWeb class as adaptor

--- phoenix-web-adapter.js
+++ phoenix-web-adapter.js
@@ -1,11 +1,22 @@
-class Window {
-  constructor() {
+export class Window {
+  id = null
+  screenId = null
+
+  constructor({ screenId, spaceId }) {
+    this.id = uuidv4()
+    this.screenId = screenId
+    this.spaceId = spaceId
+
     this._frame = {
       x: 0,
       y: 0,
       width: 0,
       height: 0,
     }
+  }
+
+  static all() {
+    return PhoenixWeb.windows
   }
 
   frame() {
@@ -26,28 +37,37 @@ class Window {
   }
 }
 
-class Key {
+export class Key {
   static on(key, modifiers, callback) {
     console.debug(`Key.on(${key}, ${modifiers}, ${callback}) called`)
   }
 }
 
-class Event {
+export class Event {
   static on(eventName, callback) {
     console.debug(`Event.on(${eventName}, ${callback}) called`)
   }
 }
 
-class Phoenix {
+export class Phoenix {
   static log(...params) {
     console.debug("Phoenix.log called")
     console.log(...params)
   }
 }
 
-class Space {
-  constructor() {
+export class Space {
+  id = null
+  screenId = null
 
+  static all() {
+    return PhoenixWeb.spaces
+  }
+
+  constructor({ screenId, spaceId }) {
+    this.id = uuidv4()
+    this.screenId = screenId
+    this.spaceId = spaceId
   }
 
   static active() {
@@ -55,17 +75,23 @@ class Space {
   }
 
   windows() {
-    return [new Window()] // TODO: Return list of instantiated windows
+    return PhoenixWeb.windows.filter((window) => window.spaceId === this.id)
   }
 }
 
-class Screen {
-  constructor() {
+export class Screen {
+  id = null
 
+  constructor() {
+    this.id = uuidv4()
   }
 
   static main() {
     return new Screen() // TODO: Return main screen
+  }
+
+  static all() {
+    return PhoenixWeb.screens
   }
 
   flippedVisibleFrame() {
@@ -76,4 +102,35 @@ class Screen {
       height: 0,
     }
   }
+
+  spaces() {
+    return PhoenixWeb.spaces.filter((space) => space.screenId === this.id)
+  }
+
+  windows() {
+    return PhoenixWeb.windows.filter((window) => window.screenId === this.id)
+  }
+}
+
+export class PhoenixWeb {
+  static spaces = []
+  static screens = []
+  static windows = []
+
+  static init() {
+    const screen = new Screen()
+    const space = new Space({ screenId: screen.id })
+    const window = new Window({ screenId: screen.id, spaceId: space.id })
+
+    this.screens.push(screen)
+    this.spaces.push(space)
+    this.windows.push(window)
+  }
+}
+
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
 }
---
+++ test/test.js
@@ -0,0 +1,36 @@
+import { expect } from 'chai'
+import { PhoenixWeb, Screen, Space, Window } from '../phoenix-web-adapter'
+
+describe('PhoenixWeb', () => {
+  it('exists', () => {
+    expect(PhoenixWeb).not.to.be.undefined
+  })
+
+  describe('.init', () => {
+    it('initializes 1 screen with 1 space, and 1 window', () => {
+      expect(Screen.all()).to.have.lengthOf(0)
+      expect(Space.all()).to.have.lengthOf(0)
+      expect(Window.all()).to.have.lengthOf(0)
+
+      PhoenixWeb.init()
+
+      expect(Screen.all()).to.have.lengthOf(1)
+      const firstScreen = Screen.all()[0]
+
+      expect(firstScreen).to.be.instanceOf(Screen)
+
+      expect(Space.all()).to.have.lengthOf(1)
+      const firstSpace = Space.all()[0]
+
+      expect(firstScreen.spaces()).to.have.lengthOf(1)
+
+      expect(Window.all()).to.have.lengthOf(1)
+      const firstWindow = Window.all()[0]
+
+      expect(firstScreen.windows()).to.have.lengthOf(1)
+      expect(firstScreen.windows()[0]).to.equal(firstWindow)
+      expect(firstSpace.windows()).to.have.lengthOf(1)
+      expect(firstSpace.windows()[0]).to.equal(firstWindow)
+    })
+  })
+})
